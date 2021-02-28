c4f0537c853404961e71d8162d2f957d039c99a2
Sarah Ma <sarahjma4@gmail.com>
2021-02-28 17:16:00 -0500

    added data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
 import React from "react";
 import header from "../assets/svg/header.svg";
+import bowl from "../assets/svg/bowl.svg";
+import styled from "styled-components";
 
 const Home = () => {
   return (
-    <div>
-      <img src={header} alt="clarklake cookbook header" />
-    </div>
+    <HomeStyle>
+      <div className="header-img">
+        <img src={header} alt="clarklake cookbook header" />
+        <img className="bowl-svg" src={bowl} alt="" />
+      </div>
+    </HomeStyle>
   );
 };
+
+const HomeStyle = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 3em 0;
+  .header-img {
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-end;
+    padding: 1em 1em 4em;
+    /* box-shadow: 0px 7px 10px -3px rgb(27 112 35 / 39%); */
+  }
+  .bowl-svg {
+    max-width: 15%;
+  }
+`;
 
 export default Home;
