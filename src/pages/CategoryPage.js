e859a110abe8578103b9f7f2937da628bfac44fd
Sarah Ma <sarahjma4@gmail.com>
2021-05-14 17:29:00 -0400

    added responsive changes to category page

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -55,11 +55,12 @@ const CategoryStyle = styled(motion.div)`
     text-transform: uppercase;
     font-size: 5em;
     letter-spacing: 5px;
+    padding: 0em 0.5em;
   }
   .card-container {
     width: 100%;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
     align-items: space-evenly;
     flex-wrap: wrap;
   }
@@ -68,6 +69,16 @@ const CategoryStyle = styled(motion.div)`
   }
   .category-card {
     padding-bottom: 3em;
+  }
+  @media only screen and (max-width: 1080px) {
+    .cat-header {
+      text-align: center;
+    }
+  }
+  @media only screen and (max-width: 770px) {
+    .cat-header {
+      font-size: 2.5em;
+    }
   }
 `;
 
